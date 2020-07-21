8907cd6703cd4c04b7fa199e45099bb0be438287
Klarissa Smith <ksmith_email@yahoo.com>
2020-07-21 14:42:07 -0700

    creating singing in functionality

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -4,61 +4,21 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
-import { api } from "../services/api";
 
-class SignUp extends Component {
-  state = {
-    error: false,
-    fields: {
-      firstname: "",
-      lastname: "",
-      email: "",
-      username: "",
-      password: "",
-    },
-  };
-
-  //HANDLES UPDATING STATE
-  handleChange = (e) => {
-    const newFields = {
-      ...this.state.fields,
-      [e.target.name]: e.target.value,
-    };
-    this.setState({ fields: newFields });
-  };
-
-  //EVENT HANDLER FOR SUBMIT
-  handleSubmit = (e) => {
-    e.preventDefault();
-    // fetch request to login
-    api.auth.signUp(this.state.fields).then((resp) => {
-      console.log(resp)
-      if (!resp.error) {
-        // calls login function from App.js if no error from fetch
-        this.props.onSignIn(resp);
-        // redirects to home page
-        this.props.history.push("/");
-      } else {
-        this.setState({ error: true });
-      }
-    });
-  };
-
+class ReviewForm extends Component {
   render() {
     return (
       <div>
         <Container className="mt-5">
           <Card className="col-sm-6">
             <Card.Body className="col-sm-6">
-              <Form className="text-center" onSubmit={this.handleSubmit}>
+              <Form className="text-center">
                 <Form.Row>
                   <Form.Group controlId="formGridEmail">
                     <Form.Label>First Name:</Form.Label>
                     <Form.Control
                       type="firstname"
-                      name="firstname"
                       placeholder="Enter first name"
-                      onChange={this.handleChange}
                     />
                   </Form.Group>
                 </Form.Row>
@@ -67,21 +27,14 @@ class SignUp extends Component {
                     <Form.Label>Last Name:</Form.Label>
                     <Form.Control
                       type="lastname"
-                      name="lastname"
                       placeholder="Enter last name"
-                      onChange={this.handleChange}
                     />
                   </Form.Group>
                 </Form.Row>
                 <Form.Row>
                   <Form.Group controlId="formGridEmail">
                     <Form.Label>Email:</Form.Label>
-                    <Form.Control
-                      type="email"
-                      name="email"
-                      placeholder="Enter your email"
-                      onChange={this.handleChange}
-                    />
+                    <Form.Control type="email" placeholder="Enter your email" />
                   </Form.Group>
                 </Form.Row>
                 <Form.Row>
@@ -89,23 +42,20 @@ class SignUp extends Component {
                     <Form.Label>Username:</Form.Label>
                     <Form.Control
                       type="username"
-                      name="username"
                       placeholder="Enter your username"
-                      onChange={this.handleChange}
                     />
                   </Form.Group>
                 </Form.Row>
                 <Form.Row>
                   <Form.Group controlId="formGridEmail">
                     <Form.Label>Password:</Form.Label>
-                    <Form.Control type="password" name="password" onChange={this.handleChange} />
-                    
+                    <Form.Control type="password" />
                   </Form.Group>
                 </Form.Row>
                 <Form.Row>
-                  <Form.Group controlId="formGridEmail">
+                  <Form.Group controlId="textarea">
                     <Form.Label>Confirm Password:</Form.Label>
-                    <Form.Control type="password" name="password" />
+                    <Form.Control type="password" />
                   </Form.Group>
                 </Form.Row>
 
@@ -121,4 +71,4 @@ class SignUp extends Component {
   }
 }
 
-export default SignUp
+export default ReviewForm;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,33 +4,67 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
+import { api } from "../services/api";
 
 class SignIn extends Component {
+  state = {
+    error: false,
+    fields: {
+      username: "",
+      password: "",
+    },
+  };
+
+  //HANDLES UPDATING STATE
+  handleChange = (e) => {
+    const newFields = {
+      ...this.state.fields,
+      [e.target.username]: e.target.value,
+    };
+    this.setState({ fields: newFields });
+  };
+
+  //EVENT HANDLER FOR SUBMIT
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // fetch request to login
+    api.auth.signIn(this.state.fields).then((resp) => {
+      if (!resp.error) {
+        // calls login function from App.js if no error from fetch
+        this.props.signIn(resp);
+        // redirects to home page
+        this.props.history.push("/");
+      } else {
+        this.setState({ error: true });
+      }
+    });
+  };
+
   render() {
     return (
       <div>
         <Container className="mt-5">
           <Card className="col-sm-6">
             <Card.Body className="col-sm-6">
-              <Form className="text-center">
-                <Form.Row>
-                  <Form.Group controlId="formGridEmail">
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control
-                      type="username"
-                      placeholder="Enter username"
-                    />
-                  </Form.Group>
-                </Form.Row>
-                <Form.Row>
-                  <Form.Group controlId="formGridEmail">
-                    <Form.Label>Password:</Form.Label>
-                    <Form.Control
-                      type="password"
-                      placeholder="Enter password"
-                    />
-                  </Form.Group>
-                </Form.Row>
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control type="username" placeholder="Enter username" />
+                  {/* <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text> */}
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter your password"
+                  />
+                </Form.Group>
+                {/* <Form.Group controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group> */}
                 <Button variant="primary" type="submit">
                   Submit
                 </Button>
